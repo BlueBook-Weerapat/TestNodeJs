027098406563fe91b4867cc550eb41bc4115b911
WeerapatMatee <56258343+Account-Weerapat@users.noreply.github.com>
2022-11-02 20:25:42 +0700

    v 2.0.0

--- index.js
+++ index.js
@@ -6,8 +6,19 @@ const server = http.createServer(render).listen(3000);
 console.log('connect server success at ' + port);
 
 function render(req, res){
+    let url = req.url
+    url = (url.endsWith('/')) ? url:url +`/`
+    let flieName;
+    switch(url){
+        case `/`:flieName ='index.html';break
+        case `/01/`:flieName ='index copy 1.html';break
+        case `/02/`:flieName ='index copy 2.html';break
+        case `/03/`:flieName ='index copy 3.html';break
+        default :flieName = 'index copy 4.html'
+
+    }
     let ctype = {'content-type': "text/html"}
-    fs.readFile('index.html', (err,content)=>{
+    fs.readFile(flieName, (err,content)=>{
         if(!err){
             res.writeHead(200,ctype)
             res.write(content)

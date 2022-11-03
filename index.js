0d776b3de0dfd1842fd2251906c268593added8c
WeerapatMatee <56258343+Account-Weerapat@users.noreply.github.com>
2022-11-03 12:46:52 +0700

    v 4.0.0

--- index.js
+++ index.js
@@ -1,22 +1,13 @@
 const http = require('http');
 const url = require('url'); 
 const fs = require('fs');
+
+const express = require('express');
+const app = express();
+
 const port = 3000
 
-const server = http.createServer(render).listen(3000);
+app.use((req, res)=>{res.send('node')}).listen(port);
+
 console.log('connect server success at ' + port);
 
-function render(req, res){
-//    let reqUrl = url.parse(req.url,true);
-        fs.readFile('index.html',(e,c)=>{
-            if(!e){
-            res.writeHead(200,{'content-type': "text/html"})
-            res.write(c)
-            }else{
-            res.writeHead(404,{'content-type': "text/html"})
-            res.write(e.message)
-            }
-            res.end()
-        }
-        )
-}

3cf8f166400a91572b0806a7f8fd6b147578a19a
WeerapatMatee <56258343+Account-Weerapat@users.noreply.github.com>
2022-11-03 19:41:02 +0700

    redirect

--- index.js
+++ index.js
@@ -4,11 +4,15 @@ const fs = require('fs');
 const path = require('path');
 
 const express = require('express');
+const { randomFillSync } = require('crypto');
 const app = express();
 
 const port = 3000
 
-app.get('/', (req, res) => {res.sendFile(path.join(__dirname,'index.html'));});
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname,'index.html'));
+    
+});
 app.get('/01', (req, res) => {res.sendFile(path.join(__dirname,'index copy 1.html'));});
 app.get('/02', (req, res) => {res.sendFile(path.join(__dirname,'index copy 2.html'));});
 app.get('/03', (req, res) => {res.sendFile(path.join(__dirname,'index copy 3.html'));});
@@ -18,7 +22,8 @@ app.get('/04', (req, res) => {res.sendFile(path.join(__dirname,'index copy 4.htm
 app.use((req, res)=>{
     res.status(200)
     res.type('text/html')
-    res.send('404');
+    // res.send('404');
+    res.redirect('/')
 }).listen(port);
 
 console.log('connect server success at ' + port);

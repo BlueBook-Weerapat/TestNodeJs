29ba9f68e2a15178fa99873ed159abc551f3934e
WeerapatMatee <56258343+Account-Weerapat@users.noreply.github.com>
2022-11-03 10:19:20 +0700

    get url path

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const url = require('url'); 
 const fs = require('fs');
 const port = 3000
 
@@ -6,26 +7,17 @@ const server = http.createServer(render).listen(3000);
 console.log('connect server success at ' + port);
 
 function render(req, res){
-    let url = req.url
-    url = (url.endsWith('/')) ? url:url +`/`
-    let flieName;
-    switch(url){
-        case `/`:flieName ='index.html';break
-        case `/01/`:flieName ='index copy 1.html';break
-        case `/02/`:flieName ='index copy 2.html';break
-        case `/03/`:flieName ='index copy 3.html';break
-        default :flieName = 'index copy 4.html'
-
-    }
-    let ctype = {'content-type': "text/html"}
-    fs.readFile(flieName, (err,content)=>{
-        if(!err){
-            res.writeHead(200,ctype)
-            res.write(content)
-        }else{
-            res.writeHead(404,ctype)
-            res.write(err.message)
-        }
-        return res.end()    
-    })
+    res.writeHead(200,{'content-type': "text/html"})
+   let reqUrl = url.parse(req.url,true);
+    // with (reqUrl){
+        res.write(`
+        path: ${reqUrl.path} <br />
+        pathname: ${reqUrl.pathname}<br />
+        search: ${reqUrl.search}<br />                                         
+        
+        `)
+        for (element in reqUrl.query){
+            res.write(`${element} : ${reqUrl.query[element]}<br />`)
+        };
+        res.end()
 }

b42e0e9574bd01bf4a248021f3a3ee0ab3f83be8
WeerapatMatee <56258343+Account-Weerapat@users.noreply.github.com>
2022-11-03 13:10:21 +0700

    v 4.0.1

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const http = require('http');
 const url = require('url'); 
 const fs = require('fs');
+const path = require('path');
 
 const express = require('express');
 const app = express();
 
 const port = 3000
 
-app.use((req, res)=>{res.send('node')}).listen(port);
+app.use((req, res)=>{
+    res.status(200)
+    res.type('text/html')
+    res.sendFile(path.join(__dirname, 'html/01.html'));
+
+
+
+}).listen(port);
 
 console.log('connect server success at ' + port);
 

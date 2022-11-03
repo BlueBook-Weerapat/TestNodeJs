07824aa9ee59e2982e90f8f940dedd34fd7353d7
WeerapatMatee <56258343+Account-Weerapat@users.noreply.github.com>
2022-11-03 12:34:32 +0700

    v 3.0.0

--- index.js
+++ index.js
@@ -7,17 +7,16 @@ const server = http.createServer(render).listen(3000);
 console.log('connect server success at ' + port);
 
 function render(req, res){
-    res.writeHead(200,{'content-type': "text/html"})
-   let reqUrl = url.parse(req.url,true);
-    // with (reqUrl){
-        res.write(`
-        path: ${reqUrl.path} <br />
-        pathname: ${reqUrl.pathname}<br />
-        search: ${reqUrl.search}<br />                                         
-        
-        `)
-        for (element in reqUrl.query){
-            res.write(`${element} : ${reqUrl.query[element]}<br />`)
-        };
-        res.end()
+//    let reqUrl = url.parse(req.url,true);
+        fs.readFile('index.html',(e,c)=>{
+            if(!e){
+            res.writeHead(200,{'content-type': "text/html"})
+            res.write(c)
+            }else{
+            res.writeHead(404,{'content-type': "text/html"})
+            res.write(e.message)
+            }
+            res.end()
+        }
+        )
 }

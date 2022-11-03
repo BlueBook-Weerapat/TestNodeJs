d51fe7a099ef235f2447f19fd388124431bc4da8
WeerapatMatee <56258343+Account-Weerapat@users.noreply.github.com>
2022-11-03 20:06:41 +0700

    static flie

--- index.js
+++ index.js
@@ -4,15 +4,12 @@ const fs = require('fs');
 const path = require('path');
 
 const express = require('express');
-const { randomFillSync } = require('crypto');
 const app = express();
 
 const port = 3000
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname,'index.html'));
-    
-});
+app.get('/', (req, res) => {res.sendFile(path.join(__dirname,'index.html'));});
+// app.get('/:data', (req, res) => {res.send('11');});
 app.get('/01', (req, res) => {res.sendFile(path.join(__dirname,'index copy 1.html'));});
 app.get('/02', (req, res) => {res.sendFile(path.join(__dirname,'index copy 2.html'));});
 app.get('/03', (req, res) => {res.sendFile(path.join(__dirname,'index copy 3.html'));});
@@ -22,8 +19,8 @@ app.get('/04', (req, res) => {res.sendFile(path.join(__dirname,'index copy 4.htm
 app.use((req, res)=>{
     res.status(200)
     res.type('text/html')
-    // res.send('404');
-    res.redirect('/')
+    res.send('404');
+    // res.redirect('/')
 }).listen(port);
 
 console.log('connect server success at ' + port);

25c076707e796bf7c278d0618fd5b97841801275
WeerapatMatee <56258343+Account-Weerapat@users.noreply.github.com>
2022-11-06 17:03:28 +0700

    install  express-session &  cookie-parser

--- index.js
+++ index.js
@@ -4,12 +4,17 @@ const fs = require('fs');
 const path = require('path');
 const ejs = require('ejs');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const sessions = require('express-sessions');
 
 const express = require('express');
+const session = require('express-session');
 const app = express();
 
 const port = 3000
 app.use(bodyParser.urlencoded({extended : true}));
+app.use(cookieParser());
+// app.use(session()
 app.use(express.static('public'));
 app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
 app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
